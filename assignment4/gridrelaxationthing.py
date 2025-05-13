29e14e567ed2c3cb783b386203ca48fc11f823b9
BWatson6 <113988911+BWatson6@users.noreply.github.com>
2025-05-13 13:48:32 +0100

    still not working

--- assignment4/gridrelaxationthing.py
+++ assignment4/gridrelaxationthing.py
@@ -47,19 +47,21 @@ class gridthing():
         # might need to change this error thing if code gets stuck in a loop
         # currently have it so that the larger grid (N) the smaller the error
         # must be.
-        while change > 1e-5/self.N**2:         
+        while change > 1e-4/self.N**2:         
             previous_grid = grid_array*1
-            for x_counter in range (1, self.N+1):
-                for y_counter in range (1, self.N+1):
-                    part1 = grid_array[x_counter+1][y_counter]+grid_array[x_counter-1][y_counter]+grid_array[x_counter][y_counter+1]+grid_array[x_counter][y_counter-1]
+            for y_counter in range (1, self.N+1):
+                for x_counter in range (1, self.N+1):
+                    part1 = grid_array[y_counter+1][x_counter]+grid_array[y_counter-1][x_counter]+grid_array[y_counter][x_counter+1]+grid_array[y_counter][x_counter-1]
                     part2 = h**2* function(X[x_counter-1], Y[y_counter-1])
-                    grid_array[x_counter][y_counter] = 0.25*(part1-part2)
-                    
+                    # grid_array[y_counter][x_counter] = 0.25*(part1+part2)
+
                     #overrelaxed_version
-                    # grid_array[x_counter][y_counter] = 0.25*omega*(part1-part2)+(1-omega)*grid_array[x_counter][y_counter]
-      
-            change = np.max(np.abs((previous_grid[1:self.N+1,1:self.N+1]-grid_array[1:self.N+1,1:self.N+1])/previous_grid[1:self.N+1,1:self.N+1]))
-            #print('change value', change)
+                    grid_array[y_counter][x_counter] = 0.25*omega*(part1+part2)+(1-omega)*grid_array[y_counter][x_counter]
+
+
+            if counter>10: # this is just to make sure that theres no divide by 0 issue wth this   
+                change = np.max(np.abs((previous_grid[1:self.N+1,1:self.N+1]-grid_array[1:self.N+1,1:self.N+1])/previous_grid[1:self.N+1,1:self.N+1]))
+            # print('change value', change)
             counter +=1
         self.previous_grid = previous_grid
         self.grid_array=grid_array
@@ -67,7 +69,7 @@ class gridthing():
         print('number of iterations', counter)
         
 
-        
+
     def plot(self):
         # after loop
         ax = plt.axes(projection='3d')
@@ -79,183 +81,156 @@ class gridthing():
         N_grid = self.grid_array[1:self.N+1,1:self.N+1]
         ax.plot_surface(X,Y, N_grid, cmap=cm.coolwarm_r)
 
-    def walker(self, walk_numb, start_point, function_grid):
-        grid_for_counts = self.grid_array*0
+
+    def walker_vesion2(self, walk_numb, start_value):
+        # input is coordinats in the form (x,y) need to also switch order to 
+        # get into indexing format
         h = (self.interval[1]-self.interval[0])/self.N
-        function_counter = 0
-
-        # location of walker at any time is defined by the variable position
-        # the input is currently an index of the location of grid (unitless)
-        # ngl I think my walker function is currently pretty fucked 
-        
-        for i in range(walk_numb):
-            # rest possition back to the start
-            position = [start_point[0],start_point[1]]
-
-            while position[0] in range(1, self.N+1) and position[1] in range(1, self.N+1):
-                # function grid is of size NxN, if position is in grid array
-                # need to switch to make it match up
-                function_counter += function_grid[position[0]-1, position[1]-1]  
-                direction = np.random.choice([1, 2, 3, 4], 
-                                             p=[0.25, 0.25, 0.25, 0.25], 
-                                             size=(1))
-                if direction==1:
-                    position = [position[0]+1, position[1]]                    
-                elif direction==2:
-                    position = [position[0], position[1]+1]
-                elif direction==3:
-                    position = [position[0]-1, position[1]]
-                else:
-                    position = [position[0], position[1]-1]
-                  
-                grid_for_counts[position[0], position[1]] += 1
-                
-            
-        distribution_list = grid_for_counts[self.boundry_index]
-        # this is probability of a walker ariving at a certain point on the boundry
-        Green_function = 1/walk_numb * distribution_list
-        # this is probabitiy of ariving at certain points on the grid times h^2
-        # it will be used for the greens grid function
-        Green_grid_function = h**2/walk_numb * function_counter
-        return Green_function, Green_grid_function # use boundry index to go to physical position on grid
-    
-    def Function_at_point(self, walk_numb, point, function):
-        # using walkers to find greens function at a point and hace the value
-        # of a function at a certain point.
-        # x and y are the spacial values for the grid 
-        X = np.linspace(self.interval[0], self.interval[1], self.N)
-        X_outer_product = np.outer(X, np.ones(len(X)))
-        grid_function_vals = function(X_outer_product, np.transpose(X_outer_product))
-        
-        
-        #1. calculate the greens function at each point
-        point = [point[0]+1, point[1]+1] # convert to grid array index
-        green_func, green_grid = self.walker(walk_numb, point, grid_function_vals)
-        # boundry greens func mutiplyed by the value at the boundry
-        point1 = np.sum(green_func*self.grid_array[self.boundry_index])
-        point2 = green_grid
-        #2 use along with values at boundry conditions to figure out the value
-        #   at a certain point.
-        
-        #3. profit
-        
-        return point1+point2
-
-    def walker_vesion2(self, walk_numb, start_index):
-        
+        print('h value', h)
+        start_index = (start_value-self.interval[0])//h
+        start_index = (start_index+1).astype(int) # the plus 1 to include the indexes of the voundry condisions
+        start_index = [start_index[1], start_index[0]] # flipping the order
+        print('Point index:', start_index)
         # assuming that the start index is in terms of the grid_array index
         global_counts = self.grid_array*0
         global_counts_squared = self.grid_array*0
-        probability_grid = self.grid_array*0
         
-
-        
+        step_conter = 0
         for i in range (0, walk_numb):
             
             position = start_index*1
             walker_counts = self.grid_array*0
             while position[0] in range(1, self.N+1) and position[1] in range(1, self.N+1):
-                direction = np.random.choice([1, 2, 3, 4], 
+                direction = np.random.choice([0, 1, 2, 3], 
                                              p=[0.25, 0.25, 0.25, 0.25], 
                                              size=(1))
                 if direction==1:
-                    position = [position[0]+1, position[1]]                    
+                    position = [position[0]+1, position[1]] # +1 in y direction                     
                 elif direction==2:
-                    position = [position[0], position[1]+1]
+                    position = [position[0], position[1]+1] # +1 in x direction
                 elif direction==3:
-                    position = [position[0]-1, position[1]]
+                    position = [position[0]-1, position[1]] # -1 in y direction
                 else:
-                    position = [position[0], position[1]-1]
-                walker_counts[position[0], position[1]] += 1
+                    position = [position[0], position[1]-1] # -1 in x direction
+                    
+                walker_counts[position[0], position[1]] += 1 # adding a step to the walk
+                step_conter += 1
+            # after a walk is finiished
+            # for greens function
+            global_counts[1:self.N+1,1:self.N+1] += h**2*walker_counts[1:self.N+1,1:self.N+1] # removed the h**2 term
+            global_counts[position[0], position[1]] += 1  # 
             
-            global_counts += walker_counts
-            global_counts_squared += walker_counts**2
-            visited_sites = np.where(walker_counts>0, 1, 0)
-            probability_grid += visited_sites
+            # for varience calculation
+            global_counts_squared[1:self.N+1,1:self.N+1] += (h**2*walker_counts[1:self.N+1,1:self.N+1])**2
+            global_counts_squared[position[0], position[1]] += 1 # 1 squared is still 1
+    
             
         
         # VAREINECE PART
-        part1 = global_counts/walk_numb
-        part2 = global_counts_squared/walk_numb
-        varience = (part2-part1**2)/walk_numb
-        print('varience is:', np.sum(varience))
-        
-        
-        # VARIENCE PART END
-        
-        return probability_grid
+        self.Greens_function = global_counts/(walk_numb)
+        Varience_step = global_counts_squared/walk_numb
+        Varience = (Varience_step-self.Greens_function**2)/walk_numb
+        # print('varience is:', np.sum(Varience))
+        # print('error is:', np.sqrt(np.sum(Varience)))
+        print('the average walk length is', step_conter/walk_numb)
+        return self.Greens_function, Varience # maybe this works??
 
     def function_val(self, walk_numb, point_index, function):
-        X = np.linspace(self.interval[0], self.interval[1], self.N)
-        X_outer_product = np.outer(X, np.ones(len(X)))
-        h = (self.interval[1]-self.interval[0])/self.N
-        grid_function_vals = function(X_outer_product, np.transpose(X_outer_product))
+        Y = np.linspace(self.interval[0], self.interval[1], self.N)
+        Y_outer_product = np.outer(Y, np.ones(len(Y)))
         
-        point_index = [point_index[0]+1, point_index[1]+1]
-        Green_array = self.walker_vesion2(walk_numb, point_index)/walk_numb
-
-
-        #laplace part
-        Green_array[self.boundry_index] = Green_array[self.boundry_index]*self.grid_array[self.boundry_index]
         
-        #possion part
-        Green_array[1:self.N+1, 1:self.N+1] = -Green_array[1:self.N+1, 1:self.N+1]*grid_function_vals*h**2
+        # grid of function values and including the boundry conditions 
+        potential_grid = self.grid_array*1
+        potential_grid[1:self.N+1,1:self.N+1] = function(np.transpose(Y_outer_product), Y_outer_product)
         
-        return np.sum(Green_array)
+        Green_array, varience_array = self.walker_vesion2(walk_numb, point_index)
+       # test_array = np.sum(potential_grid[1:self.N+1,1:self.N+1]*Green_array[1:self.N+1,1:self.N+1])
+        function_at_point = np.sum(Green_array*potential_grid)
+        
+        return function_at_point
 
 
     
     
-def Funciton(x, y):
-    return (x**2+np.cos(y))
+def function(x, y): # this one is just for testing
+    return 1
+
+def function_a(x, y):
+    return 10
+
+def function_b(x, y):
+    return y/10
+
+def function_c(x,y):
+    center = np.array([5,5])
+    r = np.sqrt((x-center[0])**2 - (y-center[1])**2)
+    return np.exp(-2000*r)
 
 
+# varience seems to small!
+# feel like im off by about 4
 
 
-N_val = 15
+N_val = 22
 initial_value = 5
-bounbry_value = 0.01
-interval = [-10, 10]
+bounbry_value = 1
+interval = np.array([0, 10])
 
-point_of_interest = [3,3]
+point_of_interest = np.array([5, 2]) # now as spacial coordinate
 
-# now for the trouble shooting :((
+
 
 
 a = gridthing(N_val, initial_value, bounbry_value, interval)
-a.grid_array[:,0]=-1
-a.grid_array[:,N_val+1]=-1
+# need to be carful not to boundry conditions to the corners!
+# a.grid_array[1:N_val+1,0]=9
+#a.grid_array[1:N_val+1,N_val+1]=-1
 
-# a.grid_array[0] = 1
-# a.grid_array[N_val+1] = 1
+# a.grid_array[0, 1:N_val+1] = 1
+# a.grid_array[N_val+1, 1:N_val+1] = 1
 
 
-# input here is for a point in NxN array but then gets converted to 
-# b = a.Function_at_point(1000, point_of_interest, Funciton)
 
-c = a.function_val(1000, point_of_interest, Funciton)
-
-# # a.grid_array[N_val][N_val]=2
-a.intergrator(Funciton)
-print('over-relaxed method:', a.grid_array[point_of_interest[0], point_of_interest[1]])
-# print('walker method 1:', b)
+c = a.function_val(5000, point_of_interest, function_a)
 print('walker method 2:', c)
+
+a.intergrator(function_a)
+# print('better_val:', a.grid_array[point_of_interest[0]+1, point_of_interest[1]+1])
 a.plot()
 
+#%%  TASK 3 don't run unless needed
+# 
+walk_number = 1000
+Greens_array1,_ = np.flip(a.walker_vesion2(walk_number, [5, 5]), axis=1)
+Greens_array2,_ = np.flip(a.walker_vesion2(walk_number, [2.5, 2.5]), axis=1)
+Greens_array3,_ = np.flip(a.walker_vesion2(walk_number, [0.1, 2.5]), axis=1)
+Greens_array4,_ = np.flip(a.walker_vesion2(walk_number, [0.1, 0.1]), axis=1)
+
+#%% this break is so that i dont need to runn the greens functions every time
+
+fig1, ([ax11, ax12], [ax13, ax14]) = plt.subplots(2, 2, figsize=(10, 10))
+Interval_List = [0, 10, 0, 10]
+ax11.imshow(Greens_array1[1:N_val+1,1:N_val+1], extent=Interval_List)
+ax12.imshow(Greens_array2[1:N_val+1,1:N_val+1], extent=Interval_List)
+ax13.imshow(Greens_array3[1:N_val+1,1:N_val+1], extent=Interval_List)
+ax14.imshow(Greens_array4[1:N_val+1,1:N_val+1], extent=Interval_List)
+
+ax11.set(title='(a) [5,5]')
+ax12.set(title='(b) [2.5, 2.5]')
+ax13.set(title='(c) [0.1, 2.5]', 
+         xlabel='cm',
+         ylabel='cm')
+ax14.set(title='(d) [0.1, 0.1]')
+fig1.colorbar(ax11.imshow(Greens_array1[1:N_val+1,1:N_val+1], extent=Interval_List))
+fig1.colorbar(ax12.imshow(Greens_array2[1:N_val+1,1:N_val+1], extent=Interval_List))
+fig1.colorbar(ax13.imshow(Greens_array3[1:N_val+1,1:N_val+1], extent=Interval_List))
+fig1.colorbar(ax14.imshow(Greens_array4[1:N_val+1,1:N_val+1], extent=Interval_List))
+
+
 '''
-am i comparing the right points from the different intergration methods?
+I need to go through all my code and make sure I'm indexing how I want to be
+BUT first... a brake
 
-for the over relaxed method I am taking the valu from the grid array wich is
-of dimentions (N+2)x(N+2)
-
-for the walker method input location in NxN grid but then gets conferted into
-the grid array index for doing any calculations.
-
-seems to be a lot of variation in the result from the walker method.
-
-
-now ned to figure out how to do the thing for the possion case coz seems to be
-in the ball park for the laplacian version
-
-how am i lossing negative values?? or maybe my over-relax is wierd
+need to advance so x and y interval are independent
 '''
